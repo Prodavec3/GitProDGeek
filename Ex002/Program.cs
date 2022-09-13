504c80a0931aa6058f16a44d41cc54c90c5fc14e
Prodavec3 <destroedst@mail.ru>
2022-09-13 21:12:13 +0300

    Ex002

--- Ex002/Program.cs
+++ Ex002/Program.cs
@@ -6,7 +6,7 @@ namespace Ex002
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("dotnet new console, git add Ex002, git commit -m 'Ex002', git push -u origin main");
         }
     }
 }

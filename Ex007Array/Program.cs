51ee35c2f42d51c88a0f75fcbd639c9e93649ee4
Prodavec3 <destroedst@mail.ru>
2022-09-26 14:16:23 +0300

    Массив

---
+++ Ex007Array/Program.cs
@@ -0,0 +1,20 @@
+﻿int[] array = { 123, 51, 6433, 231, 123, 35635, 123};
+int max = array[0];
+for (var i = 0; i < array.Length; i++)
+{
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+}
+Console.WriteLine(max);
+
+// либо
+foreach (int i in array)
+{
+    if (i > max)
+    {
+        max = array[i];
+    }
+}
+Console.WriteLine(max);

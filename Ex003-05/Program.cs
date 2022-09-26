5aa90e77cc84427b9012809027e5eb1f4e3c5489
Prodavec3 <destroedst@mail.ru>
2022-09-26 12:46:59 +0300

    Ex003-05

---
+++ Ex003-05/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel;
+float a, b, c;
+Console.WriteLine("A + B");
+a = new Random().Next(0, 10);
+b = new Random().Next(0, 10); //0..9
+Console.WriteLine("A: " + a + ", B: " + b);
+c = a + b;
+Console.WriteLine("Сумма: " + c);

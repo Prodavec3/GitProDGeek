21492a9653260a52a87f2768c9f92476913f5a89
Prodavec3 <destroedst@mail.ru>
2022-09-26 14:29:22 +0300

    add + print array

---
+++ Ex008/Program.cs
@@ -0,0 +1,23 @@
+﻿void AddArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 100);
+        index++;
+    }
+}
+
+void PrintArray(int[] collectionPrint)
+{
+    for (var i = 0; i < collectionPrint.Length; i++)
+    {
+        Console.WriteLine(collectionPrint[i].ToString());
+    }
+}
+
+int[] array = new int[10]; // 10 элементов в массиве определили, наполнен 0
+
+AddArray(array);
+PrintArray(array);

9a273502cb5d3463fe65390ececa01ad805c70bc
Prodavec3 <destroedst@mail.ru>
2022-09-27 14:24:06 +0300

    Парсинг в массив кортежей

--- Ex014/Program.cs
+++ Ex014/Program.cs
@@ -8,7 +8,7 @@ Console.WriteLine(a.GetTypeCode());
 
 // Задача увеличить координаты в 2 раза
 
-string text = "(1,2) (3,4) (5,6) (7,8)" // убираем скобки, они роли не будут играть как только мы сделаем парсинг по числам
+string text = "(1,2) (2,5) (3,4) (4,6) (5,6) (7,8)" // убираем скобки, они роли не будут играть как только мы сделаем парсинг по числам
               .Replace("(", "")
               .Replace(")", "");
 
@@ -16,6 +16,8 @@ var data = text.Split(" ")
                .Select(item => item.Split(",")) // Выбираем значение по разделителю
                .Select(e => (x: int.Parse(e[0]), y: int.Parse(e[1]))) // Массив кортежей (int, int), вывод делать WriteLine(data[i]), без этой строки был массив
                // x и y назвали данные значения, потом с ними можно будет работать 
+               .Where(e => e.x % 2 == 0) // можно сделать выборку по условию, если координата х делится на 2
+               .Select(point => (point.x * 10, point.y))
                .ToArray(); // Разбиение строки с учетом разделителя
 
 
@@ -23,7 +25,7 @@ var data = text.Split(" ")
 for(int i = 0; i < data.Length; i++)
 {
     Console.WriteLine(data[i]);
-    Console.WriteLine(data[i].x * data[i].y); // можем производить операции с Item кортежа
+    //Console.WriteLine(data[i].x * data[i].y); // можем производить операции с Item кортежа
 
     /*for (int j = 0; j < data[i].Length; j++)
     {

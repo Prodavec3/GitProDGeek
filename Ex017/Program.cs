0960f16d72833e1c61853250e2580e2f7ae96e34
Prodavec3 <destroedst@mail.ru>
2022-09-28 09:10:23 +0300

    Информация о вложенных папках и файлах в директорию с использованиием рекурсии

---
+++ Ex017/Program.cs
@@ -0,0 +1,30 @@
+﻿/* Для работы с директориями */
+string path = "/Users/ybiryukov/Desktop/Git/Ex001";
+DirectoryInfo di = new DirectoryInfo(path); // Указал путь, например, к Ex001
+Console.WriteLine(di.CreationTime); // дата создания папки
+FileInfo[] files = di.GetFiles();
+for (int i = 0; i < files.Length; i++)
+{
+    Console.WriteLine(files[i].Name);
+}
+
+void CatalogInfo(string path, string indent = "") // путь и отступы
+{
+    DirectoryInfo catalog = new DirectoryInfo(path); // инфо о директории
+    DirectoryInfo[] catalogs = catalog.GetDirectories(); // массив всех директорий
+    for (int i = 0; i < catalogs.Length; i++) // весь вывод папок
+    {
+        Console.WriteLine($"{indent}{catalogs[i].Name}");
+        CatalogInfo(catalogs[i].FullName, indent + " ");
+    }
+
+    FileInfo[] files = catalog.GetFiles(); /* весь список файлов в
+                                            * текущей директории */
+    for(int i = 0; i < files.Length; i++)
+    {
+        Console.WriteLine($"{indent}{files[i].Name}");
+    }
+}
+
+string path1 = "/Users/ybiryukov/Desktop/Git/Ex001";
+CatalogInfo(path1);

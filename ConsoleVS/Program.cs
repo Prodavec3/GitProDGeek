3e76784eebe2e5cb552de4304a75e943bfced61d
Prodavec3 <destroedst@mail.ru>
2022-09-28 12:34:18 +0300

    Зависимости

--- ConsoleVS/Program.cs
+++ ConsoleVS/Program.cs
@@ -1,25 +1,26 @@
-﻿using System;
-using System.Linq;
-using System.Text;
-using System.Net.Http;
-/* до C# 10 .Net 6.0 */
-namespace ConsoleVS
-{
-    class Program
-    {
-        static public void Main(string[] args) /* args это 
-        аргументы при .exe'шнике*/
-        {
-            if (args.Length >= 0) // если что-то есть
-            {
-                Console.WriteLine($"Hello. {args[0].Replace("-", "")}");
-                /* и, допустим, дадим exe файлу какую-либо команду
-                 * bin\Debug\net6.0\ConsoleVS.exe, 
-                 * создаем его ярлык, в строку объект пишем
-                 * -text */
-                Console.ReadKey();
-            }
+﻿//using System;
+//using System.Linq;
+//using System.Text;
+//using System.Net.Http;
+///* до C# 10 .Net 6.0 */
 
-        }
-    }
-}
+//namespace ConsoleVS
+//{
+//    class Program
+//    {
+//        static public void Main(string[] args) /* args это 
+//        аргументы при .exe'шнике*/
+//        {
+//            if (args.Length >= 0) // если что-то есть
+//            {
+//                Console.WriteLine($"Hello. {args[0].Replace("-", "")}");
+//                /* и, допустим, дадим exe файлу какую-либо команду
+//                 * bin\Debug\net6.0\ConsoleVS.exe, 
+//                 * создаем его ярлык, в строку объект пишем
+//                 * -text */
+//                Console.ReadKey();
+//            }
+
+//        }
+//    }
+//}
---
+++ ConsoleVS/Program2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ListMasters;
+
+namespace ConsoleVS
+{
+    public class Program2
+    {
+        static public void Main(string[] args)
+        {
+            ArrayMaster am = new();
+            for (int i = 0; i < 20; i++)
+            {
+                am.Add(new Random().Next(1,20));
+            }
+            for (int i = 0; i < am.GetCount(); i++)
+            {
+                Console.WriteLine(am.GetValue(i));
+            }
+            
+        }
+    }
+}

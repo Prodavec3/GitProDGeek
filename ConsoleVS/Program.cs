4aa0f9714d0cdf207acec5f92770255185d1a115
Prodavec3 <destroedst@mail.ru>
2022-09-28 12:01:45 +0300

    Приложение консольное до C# 10 .Net 6.0 + разбор входящих данных у Main

--- ConsoleVS/Program.cs
+++ ConsoleVS/Program.cs
@@ -2,21 +2,24 @@
 using System.Linq;
 using System.Text;
 using System.Net.Http;
-
-class Program
+/* до C# 10 .Net 6.0 */
+namespace ConsoleVS
 {
-    static public void Main(string[] args) /* args это 
-        аргументы при .exe'шнике*/
+    class Program
     {
-        if(args.Length >= 0) // если что-то есть
+        static public void Main(string[] args) /* args это 
+        аргументы при .exe'шнике*/
         {
-            Console.WriteLine($"Hello. {args[0].Replace("-", "")}");
-            /* и, допустим, дадим exe файлу какую-либо команду
-             * bin\Debug\net6.0\ConsoleVS.exe, 
-             * создаем его ярлык, в строку объект пишем
-             * -text */
-            Console.ReadKey();
+            if (args.Length >= 0) // если что-то есть
+            {
+                Console.WriteLine($"Hello. {args[0].Replace("-", "")}");
+                /* и, допустим, дадим exe файлу какую-либо команду
+                 * bin\Debug\net6.0\ConsoleVS.exe, 
+                 * создаем его ярлык, в строку объект пишем
+                 * -text */
+                Console.ReadKey();
+            }
+
         }
-        
     }
 }
